9bd56585872f25919aa91f1b22a57c53b4ad3d59
soyeon-ann <ansy39@naver.com>
2024-09-21 01:05:01 +0900

    Quiz 3주차(240920)

---
+++ variable.py
@@ -0,0 +1,16 @@
+string = 'Hello, World!'
+print(string[0:5])
+
+result = (7 + 3) ** 2
+print(result)
+
+A = [1, 2, 3, 4, 5]
+A.append(4)
+A.append(3)
+A.append(2)
+A.append(1)
+result = A[4:9]
+print(result)
+
+fruits = {'apple': 3, 'banana': 5, 'cherry': 2}
+print(fruits['banana'])
