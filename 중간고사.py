4db7b16f4e483d9084b2aaf99dc753ff34871772
soyeon-ann <ansy39@naver.com>
2024-10-25 02:46:25 +0900

    Update 중간고사.py

--- 중간고사.py
+++ 중간고사.py
@@ -1,8 +1,9 @@
 number = 1
+sum = 0
 
-while number <= 1000 :
-    print(number)
-    number = number + 1
-
-
+while number <= 100:
+    if number % 2 != 0:
+        sum += number
+    number += 1
+print(sum)
 
